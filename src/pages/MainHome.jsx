5bbe97aa3fc23f4348ff0494a8d7d1b61825a815
mani200307 <mani89509@gmail.com>
2023-06-04 18:55:13 +0530

    Added Home page design

--- src/pages/MainHome.jsx
+++ src/pages/MainHome.jsx
@@ -4,9 +4,14 @@ import { Link } from 'react-router-dom'
 
 const MainHome = () => {
     return (
-        <div className="flex flex-col h-screen md:flex-row">
-            <Link to='/home'><Button>Vehicle Owner</Button></Link>
-            <Link to='/store'><Button>Store Owner</Button></Link>
+        <div className="flex justify-center items-center w-full h-screen">
+            <div className='flex-col text-center'>
+                <h1 className='p-10'>MotoFix</h1>
+                <div className='flex gap-10'>
+                    <Link to='/home'><Button>Vehicle Owner</Button></Link>
+                    <Link to='/store'><Button>Store Owner</Button></Link>
+                </div>
+            </div>
         </div>
     )
 }
