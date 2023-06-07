1bcc9c48f2c15b39e34fc361a198e73d9ba004f8
mani200307 <mani89509@gmail.com>
2023-06-07 22:12:14 +0530

    Made bg style

--- src/pages/MainHome.jsx
+++ src/pages/MainHome.jsx
@@ -1,19 +1,23 @@
-import React from 'react'
-import { Button } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import backgroundImage from '../assets/bg.png';
 
 const MainHome = () => {
     return (
-        <div className="flex justify-center items-center w-full h-screen">
+        <div className="flex justify-center items-center w-full h-screen" style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: '100% 100%', backgroundRepeat: 'no-repeat', filter: 'brightness(1.2)' }}>
             <div className='flex-col text-center'>
-                <h1 className='p-10'>MotoFix</h1>
+                <h1 className='p-10 text-5xl'>MotoFix</h1>
                 <div className='flex gap-10'>
                     <Link to='/home'><Button>Vehicle Owner</Button></Link>
                     <Link to='/store'><Button>Store Owner</Button></Link>
                 </div>
             </div>
+            <footer className='fixed bottom-0'>
+                Copyrights reserved @ Mani
+            </footer>
         </div>
-    )
+    );
 }
 
-export default MainHome
+export default MainHome;
